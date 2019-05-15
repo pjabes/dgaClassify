e20f7c02398e5c352be31f813bb1b9d482d04a75
Patrick Abraham <patrick@pabes.me>
2019-05-15 19:49:34 +1000

    Modified .gitignore to not track dataset files

---
+++ model.py
@@ -0,0 +1,167 @@
+
+import random
+
+import pandas as pd
+import numpy as np
+
+import matplotlib.pyplot as plt
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.embeddings import Embedding
+from keras.layers.recurrent import LSTM
+import sklearn
+from sklearn import feature_extraction
+from sklearn.model_selection import train_test_split
+
+
+# Ingesting Benign Domains
+benignDomains_df = pd.read_csv('dataset/CISCO-top-1m.csv', names=['domain'])
+benignDomains_df['class'] = 0
+benignDomains_df['src'] = 'cisco'
+
+# Ingesting Malicious Domains
+maliciousDomains_df = pd.read_csv('dataset/dga-domains.txt', sep='\t', usecols=[0,1], names=['src', 'domain'])
+maliciousDomains_df['class'] = 1
+
+# Merging Datasets
+domains_df = pd.concat([maliciousDomains_df[0:10000], benignDomains_df[0:10000]], sort=False)
+domains_df = domains_df.sample(frac=0.5)
+
+domains_df.head()
+
+
+# ## Neural Network Preprocessing
+
+# ### Generate Character Dictionary
+
+def generate_char_dictionary(dataset):
+    """Determines the set of characters within the data """
+    
+    chars_dict = dict() # Create an empty dict
+    unique_chars = enumerate(set(''.join(dataset)))
+    
+    for i, x in unique_chars: 
+        chars_dict[x] = i + 1
+    
+    return chars_dict
+
+chars_dict = generate_char_dictionary(domains_df['domain'])
+max_features = len(chars_dict) + 1
+print(chars_dict)
+
+
+def tokenizeString(domain, chars_dict):
+    """Neural Networks require data to be tokenized as integers to work."""
+
+    tokenList = []
+
+    for char in domain:
+        tokenList.append(chars_dict[char])
+  
+    return tokenList
+    
+    
+def padToken(dataset, maxlen):
+
+    newList = [0] * maxlen
+    return newList + dataset
+
+domains_df['tokenList'] = domains_df.apply(lambda row: tokenizeString(row['domain'], chars_dict), axis=1)
+domains_df['tokenListLen'] = domains_df['tokenList'].apply(lambda x: len(x))
+
+# Calculate the largest length within the DataFrame
+maxlen = domains_df['tokenListLen'].max()
+
+display(domains_df)
+
+
+def build_model(max_features_num, maxlen):
+    """Build LSTM model"""
+    model = Sequential()
+    model.add(Embedding(max_features_num, 64, input_length=maxlen))
+    model.add(LSTM(64))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+
+    model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['binary_crossentropy','acc'])
+
+    return model
+
+
+max_epoch=25
+nfolds=10
+batch_size=128
+
+from keras.callbacks import EarlyStopping
+cb = []
+
+cb.append(EarlyStopping(monitor='val_loss', 
+                        min_delta=0, #an absolute change of less than min_delta, will count as no improvement
+                        patience=5, #number of epochs with no improvement after which training will be stopped
+                        verbose=0, 
+                        mode='auto', 
+                        baseline=None, 
+                        restore_best_weights=False))
+
+
+model = build_model(max_features, maxlen)
+
+model.summary()
+
+test = domains_df['tokenList'].values.tolist()
+
+
+tokenList = sequence.pad_sequences(test, maxlen)
+
+print(tokenList)
+new_X = domains_df['tokenList'].values
+
+new_Y = domains_df['class'].values
+
+history = model.fit(x=tokenList, y=new_Y, 
+                    batch_size=batch_size, 
+                    epochs=max_epoch, 
+                    verbose=1, 
+                    callbacks=cb, 
+                    validation_split=0.2, #
+                    validation_data=None, 
+                    shuffle=True, 
+                    class_weight=None, 
+                    sample_weight=None, 
+                    initial_epoch=0,
+                    steps_per_epoch=None, 
+                    validation_steps=None)
+
+
+plt.plot(history.history['acc']) 
+plt.plot(history.history['val_acc']) 
+plt.title('Model accuracy') 
+plt.ylabel('Accuracy') 
+plt.xlabel('Epoch') 
+plt.legend(['Train', 'Test'], loc='upper left') 
+plt.show()
+
+
+def predict(domain, maxlen=maxlen):
+    """Predict whether a given domain is malicious or benign."""
+
+    domainToken = []
+    
+    for char in domain:
+        domainToken.append(chars_dict[char])
+    print(domainToken)
+
+    tokenList = sequence.pad_sequences([domainToken], maxlen)
+    
+    result = model.predict_classes(tokenList)[0]
+    
+    if result == 0:
+        return "benign"
+    else:
+        return "malicious"
+
+
+predict('geeeeeeeeasswedwcwdcxwdcxwsxseee.com')
+
